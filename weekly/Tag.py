2349ac4de0caf5852f5858dbe06dd182b3d4285d
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-06 21:55:58 +0100

    Kommentare

--- weekly/Tag.py
+++ weekly/Tag.py
@@ -1,19 +1,23 @@
 from datetime import datetime
 from datetime import date
 
-Datum = datetime.now()
+#Definition der globalen Variable des heutigen Datums
+Datum_heute = datetime.now()
+
 def Feiertage():
     #Definition einiger Feiertage
-    Nicolaus= date(year=Datum.year, month=12, day=6)
-    Weihnachten= date(year=Datum.year, month=12, day=25)
+    Nicolaus= date(year=Datum_heute.year, month=12, day=6)
+    Weihnachten= date(year=Datum_heute.year, month=12, day=25)
     Feiertag = [Nicolaus,Weihnachten]
     #Gibt die Liste an Feiertagen zurück
     return Feiertag
-Heute = date(year = Datum.year, month=Datum.month, day=Datum.day)
+Heute = date(year = Datum_heute.year, month=Datum_heute.month, day=Datum_heute.day)
 Feiertage()
 
-Datum =   datetime.now()
-print("Heute ist der" ,Datum.day, ".",Datum.month , "im Jahr" , Datum.year)
+#Ausgabe des Heutigen Tages
+print("Heute ist der" ,Datum_heute.day, ".",Datum_heute.month , "im Jahr" , Datum_heute.year)
+
+#Kontrolle ob heute ein Feiertag ist
 if Heute in Feiertage():
     print("Heute ist ein Feiertag")
 else:
