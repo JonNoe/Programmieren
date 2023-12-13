42f063fdbfcd16206db0452dd3fda060ad239281
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-13 18:38:52 +0100

    Kommentare und Laufzeit

--- Woche 2/while.py
+++ Woche 2/while.py
@@ -1,14 +1,22 @@
 import time
 import datetime
 start_time=time.time()
+#Startknopf gedrückt
 state = bool(input("Taster gedrückt? "))
 if (state):
     state = False
-    while (state ==False):
-        Lampe = True
-        time.sleep(5)
-        if (time.time() > 10):
+    while (state ==False): #bis wieder auf true wiederholen
+        Lampe = True #Lampe würde angehen
+        time.sleep(3) #3s Lampe an
+        Lampe = False
+        time.sleep(3)
+        #Lampe blinkt im 3s takt
+
+        #wenn der Blinkvorgang länger als 20s andauert, stoppt das blinken und lampe geht aus
+        if (time.time() > 20): #wenn 
             state = True
-            print("Länger als 10 sekunden an")
-print("Start des Programms: %s" % datetime.now())
-print("Ende des Programms: %s" % datetime.now()+time.time())
+            print("Länger als 20 sekunden an")
+end_time=time.time()
+laufzeit=end_time-start_time
+print("das Programm lief %s Sekunden" % laufzeit)
+
