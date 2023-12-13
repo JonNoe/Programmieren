946fa2cb282510ed5ef644725519500968200d7a
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-13 17:34:26 +0100

    Initial Commit

---
+++ java_Programme/Sterne_nebeneinander.java
@@ -0,0 +1,22 @@
+package java_Programme;
+import java.util.Scanner;
+
+
+public static void sterne(int n){
+
+}
+
+    
+
+
+public class Sterne_nebeneinander{
+    public static void main(String[] args){
+        int sterne;
+        Scanner scannen = new Scanner(System.in);
+        System.out.print("Anzahl der Sterne eingeben: ");
+        sterne= scanner.nextInt();
+
+
+    }
+    
+}
