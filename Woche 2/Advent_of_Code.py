0d17995b77a10b72c5adbf83818053f34e7ad6a0
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-18 12:23:30 +0100

    Day 1 2019

--- Woche 2/Advent_of_Code.py
+++ Woche 2/Advent_of_Code.py
@@ -1,5 +1,22 @@
-def einlesen():
-    Eingabewerte =  open('input.txt','r')
-    print(Eingabewerte.read())
+import os
+import math
 
-einlesen()
+def einlesen():
+    Eingabewerte =  "C:\\Users\\jonin\\Documents\\Programmieren\\Programmieren Repo\\Weekly\\Programmieren\\Woche 2\\input.txt"
+    try:
+        with open(Eingabewerte,"r") as file:
+            massen = file.read()
+    except FileNotFoundError:
+        print(f"Datei {Eingabewerte} nicht gefunden")
+    return(massen)
+
+
+def Treibstoff(modulmassen):
+    ins_Treibstoff=0
+    for masse in range(0,len(einlesen())):
+        Treibstoff_pro_modul = math.floor(masse / 3)
+        ins_Treibstoff = ins_Treibstoff+Treibstoff_pro_modul
+    print(ins_Treibstoff)
+    return(ins_Treibstoff)
+        
+Treibstoff(einlesen)
