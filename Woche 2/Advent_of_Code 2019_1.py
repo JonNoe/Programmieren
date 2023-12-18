f94f8625c44390ceedeb0c4331b8ea01b6307410
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-18 14:49:57 +0100

    Korrektur Input & Antwort

--- Woche 2/Advent_of_Code 2019_1.py
+++ Woche 2/Advent_of_Code 2019_1.py
@@ -5,7 +5,7 @@ def einlesen():
     Eingabewerte =  "C:\\Users\\jonin\\Documents\\Programmieren\\Programmieren Repo\\Weekly\\Programmieren\\Woche 2\\input.txt"
     try:
         with open(Eingabewerte,"r") as file:
-            massen = file.read()
+            massen = file.readlines()
     except FileNotFoundError:
         print(f"Datei {Eingabewerte} nicht gefunden")
     return(massen)
@@ -13,10 +13,12 @@ def einlesen():
 
 def Treibstoff(modulmassen):
     ins_Treibstoff=0
-    for masse in range(0,len(einlesen())):
-        Treibstoff_pro_modul = math.floor(masse / 3)
-        ins_Treibstoff = ins_Treibstoff+Treibstoff_pro_modul
-    print(ins_Treibstoff)
+    for i in range(0,len(einlesen())):
+        zeug = einlesen()
+        zeug[i] = int(zeug[i].strip('\n'))
+        ins_Treibstoff += int(zeug[i] / 3) - 2
+   
     return(ins_Treibstoff)
         
-Treibstoff(einlesen)
+print(f"Der Weihnachtsmann braucht {Treibstoff(einlesen)} Liter Treibstoff")
+
