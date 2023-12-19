79ac45f6520e4b02e65f1b011543399e827148b9
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-19 18:26:10 +0100

    Fehler

--- Woche 2/Slicing.py
+++ Woche 2/Slicing.py
@@ -8,32 +8,42 @@ def name():
     for i in range(0,Zahl_Leer):
         Leerzeichen.append(voller_name.find(" ",i))
         
-        
-    #vorname=voller_name[:Leerzeichen].capitalize()
-    for x in Leerzeichen:
-        namen=[voller_name[:x]]
-        #name.append(voller_name[Leerzeichen[0]+1:Leerzeichen[j+1]+2].capitalize())
-    #print(vorname)
-    print(Zahl_Leer)
     print(Leerzeichen)
-    print(namen)
-
-
-def website(websites):
-    gekürzt = slice(12,-5)
-    for seite in websites:
-        print(seite[gekürzt])
+    vorname=voller_name[:Leerzeichen[0]].capitalize()
+    nachname=voller_name[Leerzeichen[0]+1:].capitalize()
+    print(vorname)
+    print(nachname)
+    
+    # for i in range(1,len(Leerzeichen)):
+    #     nachname=voller_name[Leerzeichen[i-1]:].capitalize()
+    #     print(nachname)
     
 name()
 
 
-websites=['https://www.google.com/']
-weiter=True
-while (weiter):
-    webseiten=input("Geben Sie eine Webseite ein: ")
-    websites.append(webseiten)
-    mehr_websites=input("Wollen Sie noch mehr webseiten hinzufügen? ").lower()
-    if (mehr_websites != 'ja'):
-        weiter=False
+   
+    # for x in Leerzeichen:
+    #     namen=[voller_name[:x]]
+    #     #name.append(voller_name[Leerzeichen[0]+1:Leerzeichen[j+1]+2].capitalize())
+    # #print(vorname)
+    # print(Zahl_Leer)
+    # print(Leerzeichen)
+    # print(namen)
+
+
+# def website(websites):
+#     gekürzt = slice(12,-5)
+#     for seite in websites:
+#         print(seite[gekürzt])
     
-website(websites)
+
+# websites=['https://www.google.com/']
+# weiter=True
+# while (weiter):
+#     webseiten=input("Geben Sie eine Webseite ein: ")
+#     websites.append(webseiten)
+#     mehr_websites=input("Wollen Sie noch mehr webseiten hinzufügen? ").lower()
+#     if (mehr_websites != 'ja'):
+#         weiter=False
+    
+# website(websites)
