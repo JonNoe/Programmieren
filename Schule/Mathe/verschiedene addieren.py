fdd433f3f648c4ff78a85a6ccd8f2396cad1118a
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-21 14:20:34 +0100

    änderung Ordnerstruktur

---
+++ Schule/Mathe/verschiedene addieren.py
@@ -0,0 +1,26 @@
+a = 'a'
+b= 'b'
+print(a+b)
+#print(a-b)
+c=1.7
+d=6.9
+print(c+d)
+print(c-d)
+e=[1,2,3,4,5]
+f=[1,3,5,7]
+print(e+f)
+#print(e-f)
+g= True
+h= True
+print(bool(g+h))
+print(h-g)
+i='ja'
+j='nein'
+print(i+j)
+#print(j-i)
+k = {1,2}
+l= {2,4}
+print(k-l)
+
+liste=[1,'a',5,1,2,5]
+print(set(liste))
