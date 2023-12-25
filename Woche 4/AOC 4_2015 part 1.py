fe220052c6177a143e39c424860b3cb29ef99012
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2023-12-25 23:59:53 +0100

    Initial

---
+++ Woche 4/AOC 4_2015 part 1.py
@@ -0,0 +1,9 @@
+from hashlib import md5
+from itertools import count
+code = 'ckczppom'
+for i in count(1): #count(1) = alle verschiedenen möglichkeiten ausprobieren
+    print(i)
+    testen = code + str(i) #beide zusammenhängen
+    if md5(testen.encode('utf-8')).hexdigest()[:5]=='00000': #encoden von jeder kombination in hexadezimalzahl, bis die ersten 5 Stellen 
+        print(i)
+        break
