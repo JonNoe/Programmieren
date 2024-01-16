1b575c3bafc080e97422a52a13c882f9acbdc2e8
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2024-01-16 19:30:07 +0100

    Initial commit

---
+++ Woche 7/tic-tac-toe wahl.py
@@ -0,0 +1,224 @@
+import random
+
+# Problem war den Vektor zum simulieren mit dem feldvektor gleichzusetzen --> hatten die selbe adresse und so hat sich auch das gespielte feld verändert
+# nicht nur das simulierte
+# Lösung: eine neue Liste erstellen, der man die selbe bestandteile gibt
+
+# Fehler mit schwachem bot:
+# er hat das Falsche feld gelesen und dann auch mit falschem Index eingetragen
+
+#Konstanten definiert
+SPIELER_ZEICHEN = 'X'
+COMPUTER_ZEICHEN = 'O'
+#start mit es wird gespielt
+spielen = True
+Modus = 0
+#Feld zum verändern
+feld = ['1','2','3',
+        '4','5','6',
+        '7','8','9']
+ursprüngliches_feld = ['1','2','3',
+                       '4','5','6',
+                       '7','8','9']
+aktuelles_Feld = []
+#wer spielt
+spieler_aktuell = 0
+
+def feld_erstellen():
+    
+    print(ursprüngliches_feld[0] + '|' + ursprüngliches_feld[1] + '|' + ursprüngliches_feld[2])
+    print(ursprüngliches_feld[3] + '|' + ursprüngliches_feld[4] + '|' + ursprüngliches_feld[5])
+    print(ursprüngliches_feld[6] + '|' + ursprüngliches_feld[7] + '|' + ursprüngliches_feld[8])
+    
+def feld_ausgabe(Zug_s):
+    global spieler_aktuell
+    if spieler_aktuell == 'Spieler 1':
+        zeichen = SPIELER_ZEICHEN
+    elif spieler_aktuell == 'Spieler 2':
+        zeichen = COMPUTER_ZEICHEN
+    else:
+        zeichen = COMPUTER_ZEICHEN
+        
+    feld[Zug_s] = zeichen #hier geändert -1 weg
+    print(feld[0] + '|' + feld[1] + '|' + feld[2])
+    print(feld[3] + '|' + feld[4] + '|' + feld[5])
+    print(feld[6] + '|' + feld[7] + '|' + feld[8])
+
+
+def spieler_eingabe():
+    global spielen
+    while spielen == True:
+        Zug_Spieler = input('In welchem Feld wollen Sie spielen: ').lower()
+        if Zug_Spieler == 'quit':
+            spielen = False
+            return 
+        try:
+            Zug_Spieler = int(Zug_Spieler) -1
+            if position_spielbar(Zug_Spieler):
+                print('Das Feld ist bereits belegt, bitte wählen Sie ein anderes Feld')
+                continue
+        except ValueError:
+            print('Zahl Auserhalb des zahlenbereichs 1-9')
+        if Zug_Spieler >= 0 and Zug_Spieler < 9:
+            return Zug_Spieler
+        else: 
+            print('Bitte geben Sie eine Zahl zwischen 1 und 9 ein')
+
+def position_spielbar(position):
+    if  not feld[position ] == ursprüngliches_feld[position]:
+        return True
+    return False
+    
+def Computer_Zug():
+    global aktuelles_Feld
+    bestScore = -800
+    bestmove = 0
+
+    while spieler_aktuell == 'Computer':
+        for i in range(len(feld)):
+            aktuelles_Feld.append(feld[i])
+            #print(aktuelles_Feld)
+        for key in range(len(feld)):
+            print(key)
+            if position_spielbar(key): #position_spielbar(key)
+                continue
+            else:
+                aktuelles_Feld[key] = COMPUTER_ZEICHEN
+                #print(aktuelles_Feld)
+                score = minimax(aktuelles_Feld, False)
+                aktuelles_Feld[key] = ursprüngliches_feld[key]
+                if score > bestScore:
+                    bestScore = score
+                    bestmove = key
+        Computer_Zug = bestmove
+        aktuelles_Feld = []
+        feld_ausgabe(bestmove)    
+        return Computer_Zug
+
+
+
+def minimax(feld_aktuell, isMaximizing):
+    if check_wer_gewonnen(COMPUTER_ZEICHEN):
+        return 1
+    elif check_wer_gewonnen (SPIELER_ZEICHEN):
+        return -1
+    elif Kontrolle_Unentschieden():
+        return 0
+    
+    if isMaximizing: #maximizing
+        bestScore = -800
+        for key in range(len(feld_aktuell)):
+            if feld_aktuell[key] == ursprüngliches_feld[key]:
+                feld_aktuell[key] = COMPUTER_ZEICHEN
+                score = minimax(feld_aktuell, False)
+                feld_aktuell[key] = ursprüngliches_feld[key]
+                if score > bestScore:
+                    bestScore = score
+        return bestScore
+    
+    else: #minimizing
+        bestScore = 800
+        for key in range(len(feld_aktuell)):
+            if feld_aktuell[key] == ursprüngliches_feld[key]:
+                feld_aktuell[key] = SPIELER_ZEICHEN
+                score = minimax(feld_aktuell,True)
+                feld_aktuell[key] = ursprüngliches_feld[key]
+                if score < bestScore:
+                    bestScore = score
+        return bestScore
+                
+        
+                          
+def Wechsel_Spieler(aktuell):
+    global Modus
+    global spieler_aktuell
+    if aktuell == 'Spieler 1':
+        if Modus == 1:
+            spieler_aktuell = 'Spieler 2'
+        else: 
+            spieler_aktuell = 'Computer'        
+    else:
+        spieler_aktuell = 'Spieler 1'
+
+
+def Kontrolle_Unentschieden():
+    if ((feld[0] == 'X' or feld[0] == 'O')  \
+        and (feld[1] == 'X' or feld[1] == 'O')  \
+        and (feld[2] == 'X' or feld[2] == 'O')  \
+        and (feld[3] == 'X' or feld[3] == 'O')  \
+        and (feld[4] == 'X' or feld[4] == 'O')  \
+        and (feld[5] == 'X' or feld[5] == 'O')  \
+        and (feld[6] == 'X' or feld[6] == 'O')  \
+        and (feld[7] == 'X' or feld[7] == 'O')  \
+        and (feld[8] == 'X' or feld[8] == 'O')):
+            return True
+    else: return False
+
+def check_wer_gewonnen(zeichen):
+    #Kontrolle Zeilen
+    if feld[0] == feld[1] == feld[2] == zeichen:
+        return True
+    if feld[3] == feld[4] == feld[5] == zeichen:
+        return True
+    if feld[6] == feld[7] == feld[8] == zeichen:
+        return True
+    #Kontrolle Spalten
+    if feld[0] == feld[3] == feld[6] == zeichen:
+        return True
+    if feld[1] == feld[4] == feld[7] == zeichen:
+        return True
+    if feld[2] == feld[5] == feld[8] == zeichen:
+        return True
+    #Kontrolle Diagonale
+    if feld[0] == feld[4] == feld[8] == zeichen:
+        return True
+    if feld[2] == feld[4] == feld[6] == zeichen:
+        return True
+
+
+def main():
+    print('Um zu Spielen geben Sie eine Zahl zwischen 1 und 9 ein, um im jeweiligen Feld ein X zu setzen')
+    print('Tipp: Sie können das Spiel jederzeit beeneden, indem Sie "Quit" eingeben\n')
+    global Modus
+    global spieler_aktuell
+    Modus = int(input('Möchtest du gegen einen anderen Menschen (1) oder einen Computer (2) spielen? '))
+    if Modus == 1:
+        spieler_aktuell = 'Spieler 2'
+    else: 
+        spieler_aktuell = 'Computer'
+    feld_erstellen()
+    global spielen
+    while spielen == True:
+        if spieler_aktuell == 'Spieler 1':
+            zeichen = SPIELER_ZEICHEN
+        else:
+            zeichen = COMPUTER_ZEICHEN
+        print(f'{spieler_aktuell} ist am Zug')
+        if spieler_aktuell == 'Spieler 1':
+            Zug_spieler = spieler_eingabe()             
+            feld_ausgabe(Zug_spieler)
+            print(f'\nIhr Spielzug: {Zug_spieler+1} \n') 
+        elif spieler_aktuell == 'Spieler 2':
+            Zug_spieler = spieler_eingabe()             
+            feld_ausgabe(Zug_spieler)
+            print(f'\nSpieler 2 hat {Zug_spieler+1} gespielt\n')
+            
+        else:
+            Zug_Computer = Computer_Zug()
+            #feld_ausgabe(Zug_Computer)
+            print(f'\nDer Computer hat in Feld {Zug_Computer+1} gespielt \n')
+        #print(feld)
+        #hat jemand gewonnen?
+        gewonnen = check_wer_gewonnen(zeichen)
+        if gewonnen:
+            print(f'{spieler_aktuell} hat gewonnen')
+            spielen = False
+        unentschieden = Kontrolle_Unentschieden()
+        if unentschieden:
+            print('Die Partie ist unentschieden ausgegangen')
+            spielen = False
+        #Am Ende des Zuges Spielerwechsel
+        Wechsel_Spieler(spieler_aktuell)
+        
+if __name__ == '__main__':
+    main()
