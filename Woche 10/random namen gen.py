59710d39d11d692765d05ecc3d14dc8af73eb1f0
JonNoe <jon.noe.23@lehre.mosbach.dhbw.de>
2024-02-10 23:45:21 +0100

    reihenvolge verwechselt

--- Woche 10/random namen gen.py
+++ Woche 10/random namen gen.py
@@ -3,8 +3,9 @@ import random
 def random_name():
     vornamen = ["Ichigo","Naruto","Goku","Sakura","Luffy","Hinata","Sasuke","Eren","Mikasa","Asuna","Kirito","Nami","Vegeta","Rukia","Kakashi"]
     nachnamen = [    "Uzumaki","Kurosaki","Son","Haruno","Monkey D.","Hyuga","Uchiha","Yeager","Ackerman","Yuuki","Kirigaya","Nami","Brief","Kuchiki","Hatake"]
-    return random.choice(vornamen) + ' '  + random.choice(nachnamen)
+    return random.choice(nachnamen) + ' '  + random.choice(vornamen)
 
 Anzahl = int(input('Anzahl der zu generierenden Namen eingeben: '))
 for i in range(Anzahl):
     print(random_name())
+print('\nDie Reihenfolge der Namen im Japanischen ist vertauscht, daher steht der Nachname vor dem Vornamen\n')
